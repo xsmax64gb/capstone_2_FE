8718b88d3c6ad12e4db2420d694ef9ab3b3d263a
Huy <huy111128032004@gmail.com>
2026-04-03 00:02:10 +0700

    style: standardize import formatting and add semicolons for consistency

--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { baseApi } from './api/baseApi'
-import './services/adminApi'
-import './services/authApi'
-import './services/exercisesApi'
-import './services/learnApi'
-import './services/placementApi'
-import './services/vocabulariesApi'
-import authReducer from './slices/authSlice'
-import notificationReducer from './slices/notificationSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+import "./services/adminApi";
+import "./services/authApi";
+import "./services/exercisesApi";
+import "./services/learnApi";
+import "./services/placementApi";
+import "./services/vocabulariesApi";
+import authReducer from "./slices/authSlice";
+import notificationReducer from "./slices/notificationSlice";
 
 export const store = configureStore({
   reducer: {
@@ -17,7 +17,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
